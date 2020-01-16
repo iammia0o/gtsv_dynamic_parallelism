92a8def3cad6897eae95764cf6e84591de95f5fc
Mia Huong Nguyen <maihuongnt95@gmail.com>
2020-01-16 17:15:17 +0700

    test level 1 kernel

--- main.cu
+++ main.cu
@@ -217,7 +217,7 @@ void test_gtsv_v1(int m)
 		cudaMalloc((void **)&d, sizeof(DOUBLE)*m); 
 		cudaMalloc((void **)&b, sizeof(DOUBLE)*m);
 
-		// memset(d, 0, m * sizeof(DOUBLE));
+		memset(d, 0, m * sizeof(DOUBLE));
 		// memset(dl, 0, m * sizeof(DOUBLE));
 		// memset(du, 0, m * sizeof(DOUBLE));
 	}
